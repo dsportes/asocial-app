d959a95fd02bd47b23f4347582baa79067a63d44
Daniel Sportes <daniel@sportes.fr>
2023-01-16 18:45:12 +0100

    En chantier

--- src/app/sync.mjs
+++ src/app/sync.mjs
@@ -36,13 +36,34 @@ export class OperationWS extends Operation {
   async nvSM (id, v) { // nouveaux secrets et membres
     const args = { token: session.authToken, id, v }
     const ret = this.tr(await post(this, 'ChargerMS', args))
-    return [ret.rowSecrets, ret.rowMembres]
+    this.rowSecrets = ret.rowSecrets
+    this.rowMembres = ret.rowMembres
   }
 
   async nvSCS (id, v) { // nouveaux secrets, chats, sponsorings
     const args = { token: session.authToken, id, v }
     const ret = this.tr(await post(this, 'ChargerSCS', args))
-    return [ret.rowSecrets, ret.rowChats, ret.rowSponsorings]
+    this.rowSecrets = ret.rowSecrets
+    this.rowChats = ret.rowChats
+    this.rowSponsorings = ret.rowSponsorings
+  }
+
+  async nvTribu (id) { // tribu la plus récente
+    const args = { token: session.authToken, id }
+    const ret = this.tr(await post(this, 'GetTribu', args))
+    this.nvTribu = ret.rowTribu
+  }
+
+  async nvAvatar (id) { // avatar le plus récent
+    const args = { token: session.authToken, id }
+    const ret = this.tr(await post(this, 'GetAvatar', args))
+    this.nvAvatar = ret.rowAvatar
+  }
+
+  async nvGroupe (id) { // groupe le plus récent
+    const args = { token: session.authToken, id }
+    const ret = this.tr(await post(this, 'GetGroupe', args))
+    this.nvGroupe = ret.rowGroupe
   }
 
   async finKO (e) {
@@ -55,9 +76,34 @@ export class OperationWS extends Operation {
 export class OnchangeCompta extends OperationWS {
   constructor () { super($t('OPsync')) }
 
+  async chgTribu (idt) {
+    await this.nvTribu(idt)
+    const avTribu = stores.avatar.tribu
+    if (this.nvTribu.v <= avTribu.v) { this.nvTribu = null; return }
+    // TODO
+  }
+
   async run (row) {
     try {
+      const session = stores.session
+      const avStore = stores.avatar
+      this.buf = new IDBbuffer()
+      this.compta = await compile(row)
+      this.avCompta = avStore.compta
+      if (this.compta.v <= this.avCompta.v) return
 
+      if (this.compta.idt !== this.avCompta.idt) await chtTribu(this.compta.idt)
+
+      this.buf.putIDB(row)
+
+      /* commits IDB */
+      this.buf.commitIDB()
+
+      /* Maj des stores */
+      avStore.setCompta(this.compta)
+      const chg = session.setBlocage()
+      if (chg > 1) await alerteBlocage (chg)
+      
     } catch (e) { await this.finKO(e) }
   }
 }
@@ -73,8 +119,11 @@ export class OnchangeTribu extends OperationWS {
   async run (row) {
     try {
       const session = stores.session
+      const avStore = stores.avatar
       this.buf = new IDBbuffer()
       const tribu = await compile(row)
+      if (tribu.v <= avStore.tribu.v) return
+
       this.buf.putIDB(row)
 
       /* commits IDB */
@@ -110,12 +159,12 @@ export class OnchangeGroupe extends OperationWS {
       if (groupe._zombi) {
         this.buf.purgeGroupeIDB(groupe.id) 
       } else {
-        const [nvSecrows, nvMbrows] = await this.nvSM(groupe.id, avGr.v)
-        for (const rowm of nvMbrows) {
+        await this.nvSM(groupe.id, avGr.v)
+        for (const rowm of this.rowMembres) {
           nvMbs.push(await compile(rowm)) // membre ajouté / modifié
           this.buf.putIDB(rowm)
         }
-        for (const rows of nvSecrows) {
+        for (const rows of this.rowSecrets) {
           const s = await compile(rows)
           if (session.synchro) this.buf.mapSec[s.pk] = s
           nvSecs.push(s) // secret ajouté / modifié
