0b6948112e723e00027abbde1d98ac6d24eb115b
Daniel Sportes <daniel@sportes.fr>
2023-05-23 20:19:42 +0200

    Détail

--- srvw.bat
+++ srvw.bat
@@ -1,3 +1,3 @@
 @echo off
-http-server D:\git\asocial-app\dist\pwa -p 8343 -C D:\git\asocial-test1\config\fullchain.pem -K D:\git\asocial-test1\config\privkey.pem -S -P https://test.sportes.fr:8343
+http-server D:\git\asocial-app\dist\pwa -p 443 -C D:\git\asocial-test1\config\fullchain.pem -K D:\git\asocial-test1\config\privkey.pem -S -P https://test.sportes.fr:443
 exit 0
