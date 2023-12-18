49e994dc520719764056526eb883b769790f12e6
Daniel Sportes <daniel@sportes.fr>
2023-12-18 18:18:35 +0100

    Détail

--- src/i18n/fr-FR/index.js
+++ src/i18n/fr-FR/index.js
@@ -869,7 +869,7 @@ export default {
   ESt6: 'GCDlv : Purge des sposorings et des versions des avats et groupes hors limite de validité',
   ESlog: 'Synthèses des exécutions des tâches',
   ESretry: 'Reprise {0}',
-  ESping: 'Test disponible seulement en mode Firestore (pas SQL)'.
+  ESping: 'Test disponible seulement en mode Firestore (pas SQL)',
 
   // Page people et ApercuPeople
   APtr1: 'Compte de ma tranche de quotas',
