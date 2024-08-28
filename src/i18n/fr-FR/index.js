26a9459843a09163d9ee45403adeecb069bd7516
Daniel Sportes <daniel@sportes.fr>
2024-08-28 15:26:42 +0200

    Test srv build

--- src/i18n/fr-FR/index.js
+++ src/i18n/fr-FR/index.js
@@ -208,6 +208,8 @@ export default {
   EX7004: 'Session déconnectée',
   EX7005: 'La version d\'API [{0}] gérée par le serveur est incompatble avec celle [{1}] de l\'application',
 
+  EX7010: 'Trop d\'accès parallèles à la base de données. Réessayer plus tard. Message technique: {0}',
+  EX7001: 'Incident technique d\'accès à la base de données. Peut-être de réessayer plus tard évitera ce problème. Message technique: {0}',
   EX7100: 'A priori c\'est une erreur de réseau (internet non joignable, etc.) mais ça peut aussi être le serveur distant qui n\'est pas joignable / lancé : HTTP:[{0}] - URL: [{1}] - message:[{2}]',
   EX7101: 'A priori c\'est une erreur de réseau (internet non joignable, etc.) mais ça peut aussi être le serveur distant qui n\'est pas joignable / lancé : HTTP:[{0}] - URL: [{1}] - message:[{2}]',
 
