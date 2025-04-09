cff950dbdf2b28219ff8df3e4debc351d0fbae52
Daniel Sportes <daniel@sportes.fr>
2025-04-09 19:20:32 +0200

    v2.0

--- depl.ps1
+++ depl.ps1
@@ -4,11 +4,13 @@ $dtest = "D:/git/asocial-app/dist-test"
 $dt1 = "D:/git/asocial-app/dist-t1"
 $base = "D:/git/asocial-app"
 $dist = "D:/git/asocial-app/dist/pwa"
+<#
 Copy-Item -Force -Verbose -Path $dt1/README.md -Destination $pub/
 Copy-Item -Force -Verbose -Path $dt1/services.json -Destination $pub/
 Set-Location -Path $base
 npm run build:pwa
-Remove-Item $depl\* -Recurse -Force
+#>
+Remove-Item $depl\* -Recurse -Force -Exclude ".git"
 Copy-item -Force -Verbose -Recurse $dist/* -Destination $depl
 Copy-Item -Force -Verbose -Path $dtest/README.md -Destination $pub/
 Copy-Item -Force -Verbose -Path $dtest/services.json -Destination $pub/
