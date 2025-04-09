5fa5e6e92e176e2c46699cb80e685074336febb7
Daniel Sportes <daniel@sportes.fr>
2025-04-09 20:36:07 +0200

    script depl

--- depl.ps1
+++ depl.ps1
@@ -4,15 +4,16 @@ $dtest = "D:/git/asocial-app/dist-test"
 $dt1 = "D:/git/asocial-app/dist-t1"
 $base = "D:/git/asocial-app"
 $dist = "D:/git/asocial-app/dist/pwa"
-<#
+
 Copy-Item -Force -Verbose -Path $dt1/README.md -Destination $pub/
 Copy-Item -Force -Verbose -Path $dt1/services.json -Destination $pub/
 Set-Location -Path $base
 npm run build:pwa
-#>
-Remove-Item $depl\* -Recurse -Force -Exclude ".git"
+
+Set-Location -Path $depl
+Get-ChildItem -exclude .git | Remove-Item -recurse -force
+Set-Location -Path $base
 Copy-item -Force -Verbose -Recurse $dist/* -Destination $depl
 Copy-Item -Force -Verbose -Path $dtest/README.md -Destination $pub/
 Copy-Item -Force -Verbose -Path $dtest/services.json -Destination $pub/
-Set-Location -Path $base
 Write-Output "OK"
